4da451ba6843190e8e663ebffb737884dbaf4609
Serhii Pylypchuk <sergiydjan@gmail.com>
2020-03-19 10:31:35 +0200

    Complete migration

--- migrations/20200318202130-init-user-tables.js
+++ migrations/20200318202130-init-user-tables.js
@@ -15,107 +15,103 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return new Promise(resolve => {
-    const promises = [];
-    promises.push(db.createTable('user', {
-      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true, length: 13},
+  return (async () => {
+    await db.createTable('user', {
+      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true },
       username: {type: 'string', notNull: true, length: 60},
       email: {type: 'string', notNull: true, length: 60},
       password: {type: 'string', notNull: true, length: 255},
       profile_id: {type: 'int'}
-    }));
-    promises.push(db.createTable('profile', {
-      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true, length: 13},
+    });
+    await db.createTable('profile', {
+      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true },
       name: {type: 'string', notNull: true, length: 50},
       surname: {type: 'string', notNull: true, length: 100},
       city: {type: 'string', length: 60},
       country: {type: 'string', length: 60},
       info: {type: 'string', length: 255},
       user_id: {type: 'int'}
-    }));
-    promises.push(db.addForeignKey('user', 'profile', 'user_profile',
-      {
-      'profile_id': 'id'
-      }, {
+    });
+    await db.addForeignKey('user', 'profile', 'user_profile',
+        {
+          'profile_id': 'id'
+        }, {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-    }));
-    promises.push(db.addForeignKey('profile', 'user', 'profile_user',
+        });
+    await db.addForeignKey('profile', 'user', 'profile_user',
         {
           'user_id': 'id'
         }, {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-    }));
-    promises.push(db.createTable('group', {
-      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true, length: 13},
+        });
+    await db.createTable('group', {
+      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true },
       name: {type: 'string', notNull: true, length: 50},
-      description:  {type: 'string', length: 255},
-    }));
-    promises.push(db.createTable('permission', {
-      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true, length: 13},
+      description: {type: 'string', length: 255},
+    });
+    await db.createTable('permission', {
+      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true },
       name: {type: 'string', notNull: true, length: 50},
-      description:  {type: 'string', length: 255},
-    }));
-    promises.push(db.createTable('user_group', {
-      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true, length: 13},
+      description: {type: 'string', length: 255},
+    });
+    await db.createTable('user_group', {
+      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true },
       user_id: {type: 'int', notNull: true},
       group_id: {type: 'int', notNull: true},
-    }));
-    promises.push(db.addForeignKey('user_group', 'user', 'user_group_user',
+    });
+    await db.addForeignKey('user_group', 'user', 'user_group_user',
         {
           'user_id': 'id'
         }, {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-    }));
-    promises.push(db.addForeignKey('user_group', 'group', 'user_group_group',
+        });
+    await db.addForeignKey('user_group', 'group', 'user_group_group',
         {
           'group_id': 'id'
         }, {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-    }));
-    promises.push(db.createTable('group_permission', {
-      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true, length: 13},
+        });
+    await db.createTable('group_permission', {
+      id: {type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unsigned: true },
       permission_id: {type: 'int', notNull: true},
       group_id: {type: 'int', notNull: true},
-    }));
-    promises.push(db.addForeignKey('group_permission', 'group', 'group_permission_group',
+    });
+    await db.addForeignKey('group_permission', 'group', 'group_permission_group',
         {
           'group_id': 'id'
         }, {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-    }));
-    promises.push(db.addForeignKey('group_permission', 'permission', 'group_permission_permission',
+        });
+    await db.addForeignKey('group_permission', 'permission', 'group_permission_permission',
         {
           'permission_id': 'id'
         }, {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-    }));
-    resolve(Promise.all(promises));
-  });
+        });
+  })();
 };
 
 exports.down = function(db) {
-  return new Promise(resolve => {
-    const promises = [];
-    promises.push(db.removeForeignKey('user', 'user_profile'));
-    promises.push(db.removeForeignKey('profile', 'profile_user'));
-    promises.push(db.removeForeignKey('user_group', 'user_group_user'));
-    promises.push(db.removeForeignKey('user_group', 'user_group_group'));
-    promises.push(db.removeForeignKey('group_permission', 'group_permission_group'));
-    promises.push(db.removeForeignKey('group_permission', 'group_permission_permission'));
-    promises.push(db.dropTable('user'));
-    promises.push(db.dropTable('profile'));
-    promises.push(db.dropTable('group'));
-    promises.push(db.dropTable('user_group'));
-    promises.push(db.dropTable('permission'));
-    promises.push(db.dropTable('group_permission'));
-    resolve(Promise.all(promises));
-  });
+  return (async () => {
+    await db.removeForeignKey('user', 'user_profile');
+    await db.removeForeignKey('profile', 'profile_user');
+    await db.removeForeignKey('user_group', 'user_group_user');
+    await db.removeForeignKey('user_group', 'user_group_group');
+    await db.removeForeignKey('group_permission', 'group_permission_group');
+    await db.removeForeignKey('group_permission', 'group_permission_permission');
+    await db.dropTable('user');
+    await db.dropTable('profile');
+    await db.dropTable('group');
+    await db.dropTable('user_group');
+    await db.dropTable('permission');
+    await db.dropTable('group_permission');
+  })();
 };
 
 exports._meta = {
